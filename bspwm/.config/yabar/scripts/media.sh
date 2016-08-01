2ef54e868a920f4c67744d829e0b3104b9bf67e5
DarkAce65 <tvdood99@gmail.com>
2016-07-31 23:09:47 -0400

    More work with dzen2 for a media popup

--- bspwm/.config/yabar/scripts/media.sh
+++ bspwm/.config/yabar/scripts/media.sh
@@ -4,7 +4,7 @@ font='Terminusmod:size=10'
 dimensions=$(xrandr | grep '*' | awk {'print $1'})
 screenheight=$(echo $dimensions | cut -d 'x' -f2)
 screenwidth=$(echo $dimensions | cut -d 'x' -f1)
-x=$(($screenwidth - 270))
+x=$(($screenwidth - 320))
 y=55
 
 status=$(playerctl status)
@@ -16,16 +16,17 @@ if [[ "${track}" = "" ]] || [[ "${artist}" = "" ]]; then
 	exit
 fi
 
-play="^fn(FontAwesome:size=10)^fn()"
-pause="^fn(FontAwesome:size=10)^fn()"
-prev="^fn(FontAwesome:size=10)^fn()"
-next="^fn(FontAwesome:size=10)^fn()"
+pkill -x dzen2
 
-(echo "Music";
+play="^ca(1, playerctl play; $(dirname $0)/media.sh)^fn(FontAwesome:size=9)^fn()^ca()"
+pause="^ca(1, playerctl pause; $(dirname $0)/media.sh)^fn(FontAwesome:size=9)^fn()^ca()"
+prev="^ca(1, playerctl previous; $(dirname $0)/media.sh)^fn(FontAwesome:size=9)^fn()^ca()"
+next="^ca(1, playerctl next; $(dirname $0)/media.sh)^fn(FontAwesome:size=9)^fn()^ca()"
+
+(echo "";
+echo "  Track:   ^fg(#977d5e)$track^fg()";
+echo "  Artist:  ^fg(#977d5e)$artist^fg()";
+echo "  Album:   ^fg(#977d5e)$album^fg()";
 echo "";
-echo "  Track:  ^fg(#977d5e)$track^fg()";
-echo "  Artist: ^fg(#977d5e)$artist^fg()";
-echo "  Album:  ^fg(#977d5e)$album^fg()";
-echo "  $percbar";
-echo "^p(_CENTER)^ca(1, playerctl prev)"$prev"   ^ca()^ca(1, playerctl pause)"$pause"   ^ca()^ca(1, playerctl play)"$play"   ^ca()^ca(1, playerctl next)"$next"   ^ca()";
-sleep 15) | dzen2 -fg "#dbb787" -bg "#2d2123" -fn $font -x $x -y $y -w 250 -l 7 -e 'onstart=uncollapse,hide;button3=exit' & 
+echo "                     "$prev"  "$pause"  "$play"  "$next;
+sleep 20) | dzen2 -fg "#dbb787" -bg "#2d2123" -fn $font -x $x -y $y -w 300 -l 6 -ta c -e 'onstart=uncollapse;button3=exit' & 
