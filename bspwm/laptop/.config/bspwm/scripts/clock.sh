621fcc089a6dedba86418826580ded3413779d18
DarkAce65 <tvdood99@gmail.com>
2017-02-01 08:09:37 -0500

    Change clock format

--- bspwm/laptop/.config/bspwm/scripts/clock.sh
+++ bspwm/laptop/.config/bspwm/scripts/clock.sh
@@ -12,7 +12,7 @@ geometry="${width}x${height}+${x}+${y}"
 bspc config bottom_padding $((y * 2 + height - WINDOW_GAP))
 
 while :; do
-	datetime=$(date "+%a %d %b %l:%M %p")
+	datetime=$(date "+%a %b %d %l:%M %p")
 	echo "%{c}${datetime}"
 	sleep 2
 done | lemonbar -d -n "clock" -b -g $geometry -f $FONT -f $FONT_ICON -F $FG -B $BG &
